6431a8c526995e43b2cf6f6d515b6593150820f4
manojkestur <manojkestur@gmail.com>
2020-08-30 14:06:11 +0530

    loading input and style images

--- style transfer.py
+++ style transfer.py
@@ -10,3 +10,37 @@ from keras.models import Model
 from keras.applications.vgg16 import VGG16
 import tensorflow as tf
 from scipy.optimize import fmin_l_bfgs_b
+
+ITERATIONS = 15
+CHANNELS = 3
+IMAGE_SIZE = 400
+IMAGE_WIDTH = IMAGE_SIZE
+IMAGE_HEIGHT = IMAGE_SIZE
+IMAGENET_MEAN_RGB_VALUES = [123.68, 116.779, 103.939]
+CONTENT_WEIGHT = 0.06
+STYLE_WEIGHT = 5
+TOTAL_VARIATION_WEIGHT = 0.89
+TOTAL_VARIATION_LOSS_FACTOR = 1.0
+
+input_image_path = "input.png"
+style_image_path = "style.png"
+output_image_path = "output.png"
+combined_image_path = "combined.png"
+san_francisco_image_path = "/content/drive/My Drive/input56.png"
+tytus_image_path = "/content/drive/My Drive/style56.png"
+
+def load_img(path_to_img):
+  max_dim = 512
+  img = Image.open(path_to_img)
+  long = max(img.size)
+  scale = max_dim/long
+  img = img.resize((IMAGE_WIDTH,IMAGE_HEIGHT), Image.ANTIALIAS)
+  return imgit
+
+input_image = load_img(san_francisco_image_path)
+input_image.save(input_image_path)
+input_image
+
+style_image = load_img(tytus_image_path)
+style_image.save(style_image_path)
+style_image
